5afc98c987a1cd1fbf4d218c99853b16a4723095
Ihab Salameh <isalameh@ucsd.edu>
2015-01-22 22:38:38 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,38 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
+	//	$(".jumbotron p").addClass("active");
+		$('.jumbotron h1').text("Ihab Salameh");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+function projectClick(e) { 
+	// cancel default action
+	e.preventDefault();
+	// record clicks for debugging
+	//console.log("Project clicked");
+	// if a listener exist, $(this) is the element that fired the event
+	
+	var projectTitle = $(this).find("p").text();
+   var jumbotronHeader = $(".jumbotron h1");
+   jumbotronHeader.text(projectTitle);
+
+   var containingProject = $(this).closest(".project");
+  	$(".project").fadeIn()
+	var description = $(containingProject).find(".project-description");
+   if (description.length == 0) {
+      $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
+   }
+   else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+   }
+ 
+    // In an event handler, $(this) refers to 
+    // the object that triggered the event 
+   // $(this).css("background-color", "#7fff00");
 }
